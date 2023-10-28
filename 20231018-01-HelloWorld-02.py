c69077d08c64391df30970d8cbaf4fe3db655dbe
Theebrute <148635823+Theebrute@users.noreply.github.com>
2023-10-28 21:51:02 +0200

    Add files via upload

---
+++ 20231018-01-HelloWorld-02.py
@@ -0,0 +1,18 @@
+Welcome to Cursorless!
+
+Let's start using hats
+
+so we can slice and dice   
+
+without lifting a finger    "abd" (mwallg) green magenta abd  
+
+air_backroom():
+"abd" bluew  
+(apple_pie) cheesecake !! m
+
+%
+()
+[]
+]
+<>*#^^^|$;: :,@£.-f"""~_""
+
